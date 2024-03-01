bc9f074707fb33d99dd55f7737c4248875bf794a
hessorr <hessor@appstate.edu>
2024-03-01 10:22:31 -0500

    correct filtering

--- visualize_metrics.py
+++ visualize_metrics.py
@@ -15,15 +15,19 @@ if __name__ == '__main__':
     cglearn_original_metrics = read_csv("/Users/Oliviahess/Documents/Programs/Independent Study/CG-Testing/Metrics/r_pc_learned_stable_cg_metrics.csv")
     
     # need to filter by pval, sample size, and degree (get n = 2 first)
-    pval05lcd = lcd_metrics[(lcd_metrics['pval'] == 0.05) & (lcd_metrics['degree'] == 2)]
-    pval005lcd = lcd_metrics[(lcd_metrics['pval'] == 0.005) & (lcd_metrics['degree'] == 2)]
-    pval05stable = cglearn_stable_metrics[(cglearn_stable_metrics['pval'] == 0.05) & (cglearn_stable_metrics['degree'] == 2)]
-    pval005stable = cglearn_stable_metrics[(cglearn_stable_metrics['pval'] == 0.005) & (cglearn_stable_metrics['degree'] == 2)]
-    pval05original = cglearn_original_metrics[(cglearn_original_metrics['pval'] == 0.05) & (cglearn_original_metrics['degree'] == 2)]
-    pval005original = cglearn_original_metrics[(cglearn_original_metrics['pval'] == 0.005) & (cglearn_original_metrics['degree'] == 2)]
-    ss200lcd = lcd_metrics[(lcd_metrics['samplesize'] == 200) & (lcd_metrics['degree'] == 2)]
-    ss2000lcd = lcd_metrics[(lcd_metrics['samplesize'] == 2000) & (lcd_metrics['degree'] == 2)]
-    ss200stable = cglearn_stable_metrics[(cglearn_stable_metrics['samplesize'] == 200) & (cglearn_stable_metrics['degree'] == 2)]
-    ss2000stable = cglearn_stable_metrics[(cglearn_stable_metrics['samplesize'] == 2000) & (cglearn_stable_metrics['degree'] == 2)]
-    ss200original = cglearn_original_metrics[(cglearn_original_metrics['samplesize'] == 200) & (cglearn_original_metrics['degree'] == 2)]
-    ss2000original = cglearn_original_metrics[(cglearn_original_metrics['samplesize'] == 2000) & (cglearn_original_metrics['degree'] == 2)]
+    #### pval == 0.05
+    pval05200lcd = lcd_metrics[(lcd_metrics['pval'] == 0.05) & (lcd_metrics['degree'] == 2) & (lcd_metrics['samplesize'] == 200)]
+    pval052000lcd = lcd_metrics[(lcd_metrics['pval'] == 0.05) & (lcd_metrics['degree'] == 2) & (lcd_metrics['samplesize'] == 2000)]
+    pval05200stable = cglearn_stable_metrics[(cglearn_stable_metrics['pval'] == 0.05) & (cglearn_stable_metrics['degree'] == 2) & cglearn_stable_metrics['samplesize'] == 200]
+    pval052000stable = cglearn_stable_metrics[(cglearn_stable_metrics['pval'] == 0.05) & (cglearn_stable_metrics['degree'] == 2) & cglearn_stable_metrics['samplesize'] == 2000]
+    pval05200original = cglearn_original_metrics[(cglearn_original_metrics['pval'] == 0.05) & (cglearn_original_metrics['degree'] == 2) & (cglearn_original_metrics['samplesize'] == 200)]
+    pval052000original = cglearn_original_metrics[(cglearn_original_metrics['pval'] == 0.05) & (cglearn_original_metrics['degree'] == 2) &  (cglearn_original_metrics['samplesize'] == 2000)]
+    #### pval == 0.005
+    pval005200lcd = lcd_metrics[(lcd_metrics['pval'] == 0.005) & (lcd_metrics['degree'] == 2) & (lcd_metrics['samplesize'] == 200)]
+    pval0052000lcd = lcd_metrics[(lcd_metrics['pval'] == 0.005) & (lcd_metrics['degree'] == 2) & (lcd_metrics['samplesize'] == 2000)]
+    pval005200stable = cglearn_stable_metrics[(cglearn_stable_metrics['pval'] == 0.005) & (cglearn_stable_metrics['degree'] == 2) & cglearn_stable_metrics['samplesize'] == 200]
+    pval0052000stable = cglearn_stable_metrics[(cglearn_stable_metrics['pval'] == 0.005) & (cglearn_stable_metrics['degree'] == 2) & cglearn_stable_metrics['samplesize'] == 2000]
+    pval005200original = cglearn_original_metrics[(cglearn_original_metrics['pval'] == 0.005) & (cglearn_original_metrics['degree'] == 2) & (cglearn_original_metrics['samplesize'] == 200)]
+    pval0052000original = cglearn_original_metrics[(cglearn_original_metrics['pval'] == 0.005) & (cglearn_original_metrics['degree'] == 2) & (cglearn_original_metrics['samplesize'] == 2000)]
+
+
