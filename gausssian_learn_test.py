af29ac5284f7ba24c4dd5f2aa21ef1f6dc8c1ad2
hessorr <hessor@appstate.edu>
2024-03-02 17:35:38 -0500

    finish set up

--- gausssian_learn_test.py
+++ gausssian_learn_test.py
@@ -13,11 +13,11 @@ def create_cov(adj:np.array) -> np.array:
     cov = np.cov(adj, rowvar=False, bias=True)
     return cov
 
-def learn(cg:np.array, cov:float, n:int, p:float) -> dict:
+def learn(truecg:np.array, cov:float, n:int, p:float) -> dict:
     pc_skel = gaussian_pc_learn(cov, n, p, algMethod="stable")
     pat = learn_complex_norm(pc_skel, cov, n, p)
     # should i use the pattern csv or use this function? 
-    retVal = comp_pat(get_pattern_matrix(cg), pat)
+    retVal = comp_pat(get_pattern_matrix(truecg), pat)
     return retVal
 
 if __name__ == "__main__":
@@ -30,15 +30,15 @@ if __name__ == "__main__":
                     # read in the adjacency matrix cg_", i,"_50/cg50_", i,"_", j, "_data_", n,".csv
                     truecg = create_amat("/Users/Oliviahess/Documents/Programs/Independent Study/CG-Testing/cg_" + str(i) + "_50/cg50_" + str(i) + "_" + str(j) + ".csv")
                     datatruecg = create_amat("/Users/Oliviahess/Documents/Programs/Independent Study/CG-Testing/cg_" + str(i) + "_50/cg50_" + str(i) + "_" + str(j) + "_data_" + str(n) + ".csv")
-                    patterntruecg = create_amat("/Users/Oliviahess/Documents/Programs/Independent Study/CG-Testing/cg_" + str(i) + "_50/cg50_" + str(i) + "_" + str(j) + ".csv")
+                    # patterntruecg = create_amat("/Users/Oliviahess/Documents/Programs/Independent Study/CG-Testing/cg_" + str(i) + "_50/cg50_" + str(i) + "_" + str(j) + "_pattern.csv")
                     # find covariance of dataset to send into learning function
                     cov = create_cov(datatruecg)
                     # learn the pattern and then compare the pattern, getting the metrics back
                     metrics = learn(truecg, cov, n, p)
                     # declare name of cg
-                    name = ''
+                    name = "cg50_" + str(i) + "_" + str(j)
                     # add metrics to dataframe
                     pLearnedMetrics.loc[len(pLearnedMetrics)] = {'name' : name, 'pval' : p, 'samplesize' : n, 'degree' : i, 'TPR' : metrics['tpr'], 'TDR' : metrics['tdr'], 'FPR' : metrics['fpr'], 'ACC' : metrics['acc'], 'SHD' : metrics['shd']}
 
-    pLearnedMetrics.to_csv("python_pc_original_learned_metrics.csv", index=False)
+    # pLearnedMetrics.to_csv("python_pc_original_learned_metrics.csv", index=False)
 
